9fa1534ed931d1eb7b98c659ea9537107d61e510
Dylan Elliott-Hart <dylan_elliott-hart@brown.edu>
2019-05-08 03:23:45 -0400

    build knn aplets

--- shiny_app/reactive_KNN_graph/server.R
+++ shiny_app/reactive_KNN_graph/server.R
@@ -12,6 +12,44 @@ library(shiny)
 # Define server logic required to draw a histogram
 function(input, output) {
   
+  output$county_selector <- renderUI({
+    counties <- get_counties_in_state(input$state)
+    selectInput("county",
+                "Select county",
+                choices = counties,
+                multiple = FALSE)
+  })
+  
+  output$display_feature_selector <- renderUI({
+    ft_options <- input$features
+    selectInput("display_feature",
+                "Select Feature to Display",
+                choices = ft_options,
+                multiple = FALSE)
+  })
 
+  output$neighbors <- renderPlot({
+    display_neighbors_for_county(input$K,input$features,input$county, input$state, input$display_feature)
+    })
+  
+  output$full_model <- renderPlot({
+    display_KNN(input$K,input$features)
+  })
+  
+  output$message <- renderText({
+    attributes_used <- c()
+    for (i in 1:length(input$features)){
+      attributes_used <- c(attributes_used,input$features[i])
+      if (i < length(input$features)){
+        attributes_used <- c(attributes_used,', and')
+      }
+    }
+    attributes_used <- paste0(attributes_used)
+    attributes_used <- str_c(attributes_used)
+    
+    print(attributes_used)
+    paste('Modeling the 2016 Election by looking at the ', input$K, 
+          " counties most similar in terms of ", attributes_used)
+  })
 }
   
